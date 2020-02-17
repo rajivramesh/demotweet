78d7a46d0dfbe1d2b87653cd3303afb4759e5465
rajivchaurisia341@gmail.com <rajivchaurisia341@gmail.com>
2020-02-17 16:29:46 +0530

    crete api for tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,4 +3,15 @@ class TweetsController < ApplicationController
   def index
     @tweets = @current_user.followers.joins(:tweets).where("tweets.user_id IN (?)", @current_user.followers.pluck(:following_id))
   end
+
+  def create
+    if params[:tweet][:message].blank?
+      render json: {error: 'Please proivde valid parameters'}
+      return
+
+    end
+    tweet = @current_user.tweets.create(message: params[:tweet][:message])
+
+    render json: {id: tweet.id, message: tweet.message}
+  end
 end
